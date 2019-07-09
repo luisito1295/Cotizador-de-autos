c22a2e7ee8f71da7cd3bd6818826ebf179bc10a8
luisito1295 <luislopez121963@hotmail.com>
2019-07-08 23:18:14 -0500

    Cotizador de seguro de autos

---
+++ src/componentes/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = (props) => {
+    return (
+        <header className="top">
+            <h1>Cotizador de seguro de auto</h1>
+        </header>
+    )
+}
+
+export default Header;
